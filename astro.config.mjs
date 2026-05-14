cf64733fe222da1bdf2ca6bb49ad29fd40e3a402
dcl <zhan_deng@163.com>
2026-05-14 16:54:58 +0800

    修改配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://my-blog-zeta-woad-79.vercel.app',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
